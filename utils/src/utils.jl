68a32d3357cfdf8852729d90f6636bb179da4af8
Tobias Becker <tobias.becker@methodpark.de>
2020-12-05 18:44:27 +0100

    Also make a something_nothing

--- utils/src/utils.jl
+++ utils/src/utils.jl
@@ -1,5 +1,6 @@
 module utils
 export @something
+export @something_nothing
 
 function _something_impl(thing)
     :(something($(esc(thing))))
@@ -16,8 +17,34 @@ function _something_impl(thing, rest...)
     end
 end
 
+function _something_nothing_impl(thing)
+    quote
+        local evaluated = $(esc(thing))
+        if isa(evaluated, Some)
+            evaluated.value
+        else
+            evaluated
+        end
+    end
+end
+
+function _something_nothing_impl(thing, rest...)
+    quote
+        local evalued = $(esc(thing))
+        if isnothing(evalued)
+            $(_something_nothing_impl(rest...))
+        else
+            something(evalued)
+        end
+    end
+end
+
 macro something(things...)
     _something_impl(things...)
+end
+
+macro something_nothing(things...)
+    _something_nothing_impl(things...)
 end
 
 end
--- utils/test/runtests.jl
+++ utils/test/runtests.jl
@@ -11,11 +11,17 @@ using Test
     @testset "unwrapping" begin
         @test (@something Some(0)) == 0
         @test (@something nothing Some(0)) == 0
-        @test (@something Some(nothing) 1) === nothing
         @test (@something nothing nothing Some(0)) == 0
-        @test (@something nothing Some(nothing) 1) === nothing
         @test (@something nothing nothing Some(0) nothing) == 0
-        @test (@something nothing Some(nothing) 1 nothing) === nothing
+        @test isnothing(@something Some(nothing) 1)
+        @test isnothing(@something nothing Some(nothing) 1)
+        @test isnothing(@something nothing Some(nothing) 1 nothing)
+    end
+
+    @testset "wrapped nothing at the end" begin
+        @test isnothing(@something Some(nothing))
+        @test isnothing(@something nothing Some(nothing))
+        @test isnothing(@something nothing nothing Some(nothing))
     end
 
     @testset "escaping" begin
@@ -23,5 +29,36 @@ using Test
 
         @test_throws DomainError (@something sqrt(1 - val))
         @test (@something nothing (1 - val)) == -1
+    end
+end
+
+@testset "@something_nothing" begin
+    @testset "doesn't throw" begin
+        @test isnothing(@something_nothing nothing)
+        @test isnothing(@something_nothing nothing nothing)
+        @test isnothing(@something_nothing nothing nothing nothing)
+    end
+
+    @testset "unwrapping" begin
+        @test (@something_nothing Some(0)) == 0
+        @test (@something_nothing nothing Some(0)) == 0
+        @test (@something_nothing nothing nothing Some(0)) == 0
+        @test (@something_nothing nothing nothing Some(0) nothing) == 0
+        @test isnothing(@something_nothing Some(nothing) 1)
+        @test isnothing(@something_nothing nothing Some(nothing) 1)
+        @test isnothing(@something_nothing nothing Some(nothing) 1 nothing)
+    end
+
+    @testset "wrapped nothing at the end" begin
+        @test isnothing(@something_nothing Some(nothing))
+        @test isnothing(@something_nothing nothing Some(nothing))
+        @test isnothing(@something_nothing nothing nothing Some(nothing))
+    end
+
+    @testset "escaping" begin
+        val = 2
+
+        @test_throws DomainError (@something_nothing sqrt(1 - val))
+        @test (@something_nothing nothing (1 - val)) == -1
     end
 end
