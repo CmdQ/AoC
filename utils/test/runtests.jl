376972f5b645930fa200317ff3c536679840095d
Tobias Becker <tobias.becker@methodpark.de>
2020-12-05 18:27:16 +0100

    Make macro helper simpler

--- utils/test/runtests.jl
+++ utils/test/runtests.jl
@@ -1,3 +1,4 @@
+using utils
 using Test
 
 @testset "@something" begin
@@ -8,12 +9,19 @@ using Test
     end
 
     @testset "unwrapping" begin
-        @test (@something Some(0)) === 0
-        @test (@something nothing Some(0)) === 0
+        @test (@something Some(0)) == 0
+        @test (@something nothing Some(0)) == 0
         @test (@something Some(nothing) 1) === nothing
-        @test (@something nothing nothing Some(0)) === 0
+        @test (@something nothing nothing Some(0)) == 0
         @test (@something nothing Some(nothing) 1) === nothing
-        @test (@something nothing nothing Some(0) nothing) === 0
+        @test (@something nothing nothing Some(0) nothing) == 0
         @test (@something nothing Some(nothing) 1 nothing) === nothing
+    end
+
+    @testset "escaping" begin
+        val = 2
+
+        @test_throws DomainError (@something sqrt(1 - val))
+        @test (@something nothing (1 - val)) == -1
     end
 end
