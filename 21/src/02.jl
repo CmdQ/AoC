8f49f5c3c0455384d61d6b5ea354643a2fb07515
Tobias Becker <tobias.becker@methodpark.de>
2021-12-01 09:14:15 +0100

    Solve day 1

---
+++ 21/src/02.jl
@@ -0,0 +1,23 @@
+using Utils
+
+using Chain
+import IterTools
+
+inputfile = "$(replace(@__FILE__, r".jl$" => "")).txt"
+input = @chain inputfile slurp per_line_parse
+
+function one(input)
+    sum((@view input[2:end]) .> (@view input[1:end-1]))
+end
+
+function two(input)
+    @chain input begin
+        IterTools.partition(3, 1)
+        map(sum, _)
+        one
+    end
+end
+
+for f in [one, two]
+    @chain input f println
+end
