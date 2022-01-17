e636b2e3e90371919087c1055c19288ebb7f893c
Tobias Becker <tobias.becker@methodpark.de>
2022-01-17 14:04:20 +0100

    Solve day 16

--- 21/src/16.jl
+++ 21/src/16.jl
@@ -2,24 +2,11 @@ using Utils
 
 using Chain
 using CompositeStructs
+
 import IterTools
 
 const file = find_input(@__FILE__)
 const input = file |> slurp
-
-function bitvector(input::AbstractString)
-    @assert length(input) % 2 == 0 "input has length $(length(input))"
-    bytes = @chain input IterTools.partition(_, 2) map(c -> parse(UInt8, join(c), base=16), _)
-    re = BitVector((byte & (1 << i)) != 0 for byte=bytes for i=7:-1:0)
-    append!(re, [false, false, false])
-    re
-end
-@assert bitvector("D2")[1:8] == [true, true, false, true, false, false, true, false]
-@assert bitvector("FE")[1:8] == [true, true, true, true, true, true, true, false]
-@assert bitvector("FED2")[1:16] == [true, true, true, true, true, true, true, false, true, true, false, true, false, false, true, false]
-
-decimal(bits::AbstractArray{Bool}) = foldl((l,r) -> (l << 1) | r, bits, init=0)
-@assert decimal([true,true,false]) == 6
 
 struct Packet
     version::UInt8
@@ -27,7 +14,17 @@ struct Packet
     content::Union{Int,Vector{Packet}}
 end
 
-function decode(bv::BitVector, pos=1; limit=-1)
+function bitvector(input::AbstractString)
+    @assert length(input) % 2 == 0 "input has length $(length(input))"
+    bytes = @chain input IterTools.partition(_, 2) map(c -> parse(UInt8, join(c), base=16), _)
+    re = BitVector((byte & (1 << i)) != 0 for byte=bytes for i=7:-1:0)
+    # Make sure there's enought to read packets of 4.
+    push!(re, false, false, false)
+end
+
+decimal(bits::AbstractArray{Bool}) = foldl((l,r) -> (l << 1) | r, bits, init=0)
+
+function decode(bv::BitVector, limit, pos=1)::Tuple{Int,Vector{Packet}}
     found = Packet[]
     while pos <= length(bv) && limit != 0
         version = decimal(bv[pos:pos+2])
@@ -47,29 +44,23 @@ function decode(bv::BitVector, pos=1; limit=-1)
             if bv[pos]
                 len = decimal(bv[pos+1:pos+11])
                 pos += 1 + 11
-                inc, children = decode(bv[pos:end]; limit=len)
+                inc, children = decode(bv[pos:end], len)
             else
                 len = decimal(bv[pos+1:pos+15])
                 pos += 1 + 15
-                inc, children = decode(bv[pos:pos+len-1])
+                inc, children = decode(bv[pos:pos+len-1], -1)
             end
-            return pos + inc - 1, [Packet(version, type, children)]
+            pos += inc - 1
+            push!(found, Packet(version, type, children))
         end
-        if limit > 0
-            limit -= 1
-        end
+        limit > 0 && (limit -= 1)
     end
     pos, found
 end
-decode(string::AbstractString) = string |> bitvector |> decode
-decode("620080001611562C8802118E34")
-decode("620080001611562C8802118E34" |> bitvector)
 
-decode("EE00D40C823060")
-decode("38006F45291200")
-decode("C0015000016115A2E0802F182340")
+decode(string::AbstractString) = @chain string bitvector decode(1)
 
-function sumversions(paket::Packet)
+function sumversions(paket::Packet)::Int
     re = paket.version
     if !isa(paket.content, Number)
         re += map(sumversions, paket.content) |> sum
@@ -77,25 +68,56 @@ function sumversions(paket::Packet)
     re
 end
 
-sumversions(pakets::Vector{Packet}) = map(sumversions, pakets)
-
-function part1(input)
+function part1(input)::Int
     @chain input begin
         decode
         _[2]
+        only
         sumversions
         sum
-        convert(Int, _)
     end
 end
 
-@assert part1("8A004A801A8002F478") == 16
-@assert part1("620080001611562C8802118E34") == 12
-@assert part1("C0015000016115A2E0802F182340") == 23
-@assert part1("A0016C880162017C3686B18A3D4780") == 31
+assertequal(part1(input), 965)
 
-function part2(input)
+function part2(pak::Packet)::Int
+    pak.type == 4 && return pak.content
+    evald = part2.(pak.content)
+    if pak.type == 0
+        sum(evald)
+    elseif pak.type == 1
+        prod(evald)
+    elseif pak.type == 2
+        minimum(evald)
+    elseif pak.type == 3
+        maximum(evald)
+    else
+        @assert length(evald) == 2
+        left, right = evald
+        bool = if pak.type == 5
+            left > right
+        elseif pak.type == 6
+            left < right
+        elseif pak.type == 7
+            left == right
+        end
+        convert(Int, bool)
+    end
 end
 
-assertequal(part2(input))
+part2(input) = decode(input)[2] |> only |> part2
 
+assertequal(part2(input), 116672213160)
+
+using Test
+
+@testset "utilities" begin
+    @test bitvector("D2")[1:8] == [true, true, false, true, false, false, true, false]
+    @test bitvector("FE")[1:8] == [true, true, true, true, true, true, true, false]
+    @test bitvector("FED2")[1:16] == [true, true, true, true, true, true, true, false, true, true, false, true, false, false, true, false]
+    @test decimal([true,true,false]) == 6
+    @test part1("8A004A801A8002F478") == 16
+    @test part1("620080001611562C8802118E34") == 12
+    @test part1("C0015000016115A2E0802F182340") == 23
+    @test part1("A0016C880162017C3686B18A3D4780") == 31
+end
