0c7709d66a1ae8d3ccf38dc9f1513e538f696c08
Tobias Becker <nullpointeracception@gmail.com>
2025-09-26 21:13:45 -0700

    Solve day 8 with a real language.

--- 16/day8.rkt
+++ 16/day8.rkt
@@ -1,56 +1,55 @@
-#lang racket
+#lang racket/base
 
-(require threading)
+(require racket/port)
+(require racket/match)
+(require racket/list)
+
+(require "utils.rkt")
 (require "matrix.rkt")
 
 (define width 50)
 (define height 6)
 (define screen (make-matrix height width 0))
 
-(define (parse-line line)
-  (cond
-    [(regexp-match #px"rect (\\d+)x(\\d+)" line)
-     => (λ (m)
-          (let ([snd (string->number (second m))]
-                [trd (string->number (third m))])
-            (λ () (rect! screen snd trd))))]
-    [(regexp-match #px"rotate row y=(\\d+) by (\\d+)" line)
-     => (λ (m)
-          (let ([snd (string->number (second m))]
-                [trd (string->number (third m))])
-            (λ () (rotate-row! screen snd trd))))]
-    [(regexp-match #px"rotate column x=(\\d+) by (\\d+)" line)
-     => (λ (m)
-          (let ([snd (string->number (second m))]
-                [trd (string->number (third m))])
-            (λ () (rotate-col! screen snd trd))))]))
-
-(define input (~> "input8.txt"
-                  file->lines
-                  (map parse-line _)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Part 1
-
-(define (rect! scr w h)
+(define (rect! w h)
   (for* ([r (in-range h)]
          [c (in-range w)])
-    (matrix-set! scr r c 1)))
+    (matrix-set! screen r c 1)))
 
-(define (rotate-row! scr r amount)
-  (define temp (for/vector ([i (in-range width)]) (matrix-ref scr r (remainder (+ i (- width amount)) width))))
+(define (rotate-row! r amount)
+  (define temp (for/vector ([i (in-range width)]) (matrix-ref screen r (remainder (+ i (- width amount)) width))))
   (for ([i (in-range width)])
-    (matrix-set! scr r i (vector-ref temp i))))
+    (matrix-set! screen r i (vector-ref temp i))))
 
-(define (rotate-col! scr c amount)
-  (define temp (for/vector ([i (in-range height)]) (matrix-ref scr (remainder (+ i (- height amount)) height) c)))
+(define (rotate-column! c amount)
+  (define temp (for/vector ([i (in-range height)]) (matrix-ref screen (remainder (+ i (- height amount)) height) c)))
   (for ([i (in-range height)])
-    (matrix-set! scr i c (vector-ref temp i))))
+    (matrix-set! screen i c (vector-ref temp i))))
 
-(define (solve1 input)
-  (for-each (λ (thunk) (thunk)) input)
+(define/match (parse line)
+  [((pregexp #px"rect (\\d+)x(\\d+)"
+             (list _ rows cols)))
+   (shadow-as ([string->number rows cols])
+     `(rect! ,rows ,cols))]
+  [((pregexp #px"rotate (row y|column x)=(\\d+) by (\\d+)"
+             (list _ what num amount)))
+   (shadow-as ([string->number num amount])
+     (define fname
+       (string->symbol
+        (string-append "rotate-"
+                       (substring what 0 (- (string-length what) 2))
+                       "!")))
+     `(,fname ,num ,amount))]
+  [("") #f])
+
+(define (read-syntax path port)
+  (define src-datums (filter-map parse (port->lines port)))
+  (define module-datum `(module day8-mod "day8.rkt"
+                          ,@src-datums))
+  (datum->syntax (quote-syntax here) module-datum))
+
+(define (solve1)
   (for/sum ([e (in-matrix screen)]) e))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Part 2
 
 (define (solve2)
   (for ([(c i) (in-indexed (in-matrix screen))])
@@ -58,11 +57,22 @@
       (displayln ""))
     (display (if (zero? c) " " "#"))))
 
-(module+ test ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Tests
-  (require rackunit)
-  
-  (test-begin
-   (test-case "Part 1"
-              (check-equal? (solve1 input) 121))
-   (test-case "Part 2"
-              (check-not-equal? (solve2) "RURUCEOEIL"))))
+(define-syntax day8-module-begin
+  (syntax-rules ()
+    [(_ body ...)
+     (#%module-begin
+      (require rackunit)
+      body ...
+      (define answer1 (solve1))
+      (printf "Answer 1: ~A~%Answer 2:" answer1)
+      (solve2)
+      (displayln "")
+      (check-equal? answer1 121))]))
+
+(provide read-syntax
+         rect!
+         rotate-row!
+         rotate-column!
+         (rename-out (day8-module-begin #%module-begin))
+         #%app
+         #%datum)
