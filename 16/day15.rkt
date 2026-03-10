44aa962d5248573030753a6d1aa2b0f8ccbb6d93
Tobias Becker <nullpointeracception@gmail.com>
2026-03-09 17:12:20 -0700

    Solve day 15.

---
+++ 16/day15.rkt
@@ -0,0 +1,61 @@
+#lang racket
+
+(require threading)
+
+(define input
+  (for/list ([line (file->lines "input15.txt")])
+    (match line
+      [(pregexp #px"Disc #\\d has (\\d+) positions; at time=0, it is at position (\\d+)." (list _ mod rem))
+       (~> (list rem mod)
+           (map string->number _)
+           (apply cons _))])))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Part 1
+
+;; Returns gcd(a,b) and Bézout coefficients s, t such that a·s + b·t = gcd(a,b).
+;; s is the modular inverse of a mod b when gcd = 1.
+(define (egcd a b)
+  (let loop ([a a]
+             [b b]
+             [x 0]
+             [y 1]
+             [u 1]
+             [v 0])
+    (cond
+      [(zero? a)
+       (values b x y)]
+      [else
+       (define-values (q r) (quotient/remainder b a))
+       (loop r a u v (- x (* u q)) (- y (* v q)))])))
+
+(define (solve1 input)
+  (define as (map car input))
+  (define mods (map cdr input))
+  (define N (foldl * 1 mods))
+  (define ys (map (lambda~> (/ N _)) mods))
+  (define zs (map (λ (yi mi) (match-define-values (_ b _) (egcd yi mi)) b) ys mods))
+  (modulo (for/sum ([a as]
+                    [i (in-naturals 1)]
+                    [y ys]
+                    [z zs])
+            (* (- 0 a i) y z)) N))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Part 2
+
+(define (solve2 input)
+  (define longer (append input '((0 . 11))))
+  (solve1 longer))
+
+(module+ test ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Tests
+  (require rackunit)
+
+  (test-begin
+   (test-case "Part 1"
+              (check-equal? (solve1 '((4 . 5) (1 . 2))) 5)
+              (check-equal? (solve1 input) 376777))
+   (test-case "Part 2"
+              (check-equal? (solve2 input) 3903937))))
+
+(module+ main
+  (printf "Part one: ~A~%" (solve1 input))
+  (printf "Part two: ~A~%" (solve2 input)))
